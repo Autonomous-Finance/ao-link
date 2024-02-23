e5bb859e1d1afdb0a7cf6280603cb78c4b30579c
Daniel Constantin <hello@danielconstantin.net>
2024-02-23 14:05:19 +0200

    Fix broken link

--- src/app/entity/[slug]/ProcessPage.tsx
+++ src/app/entity/[slug]/ProcessPage.tsx
@@ -98,7 +98,7 @@ export function ProcessPage(props: ProcessPageProps) {
                 <IdBlock
                   label={truncateId(owner)}
                   value={owner}
-                  href={`/owner/${owner}`}
+                  href={`/entity/${owner}`}
                 />
               }
             />
