5516c9f03b1511ebf6081399b9a73367fda73f1c
Daniel Constantin <hello@danielconstantin.net>
2024-03-28 15:31:39 +0200

    Show more messages on the entity page

--- src/services/messages-api.ts
+++ src/services/messages-api.ts
@@ -17,8 +17,9 @@ export async function getMessagesByEntityId(
       .from("ao_events")
       .select("owner,id,tags_flat,target,owner_address,height,created_at")
       .order("created_at", { ascending })
-      .or(`owner_address.eq.${entityId},target.eq.${entityId}`)
-    //tags_flat ->> Forwarded-For.eq.${entityId},tags_flat ->> From-Process.eq.${entityId},
+      .or(
+        `owner_address.eq.${entityId},target.eq.${entityId},tags_flat ->> Forwarded-For.eq.${entityId},tags_flat ->> From-Process.eq.${entityId},tags_flat ->> Pushed-For.eq.${entityId}`,
+      )
 
     supabaseRq = supabaseRq.range(skip, skip + limit - 1).returns<AoEvent[]>()
 
