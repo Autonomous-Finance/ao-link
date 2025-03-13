16cfbf77ea48c3c4ca0480c9b959179192ca9678
BenAzlay <benjamin.azoulay@protonmail.com>
2025-03-13 17:25:44 +0200

    Total fee added + swap util methods

--- src/app/swap/[slug]/SwapPage.tsx
+++ src/app/swap/[slug]/SwapPage.tsx
@@ -1,36 +1,24 @@
-import { Box, CircularProgress, Paper, Stack, Tabs, Tooltip, Typography } from "@mui/material"
+import { Stack, Tooltip, Typography } from "@mui/material"
 
-import Grid2 from "@mui/material/Unstable_Grid2/Grid2"
-import { MessageResult } from "@permaweb/aoconnect/dist/lib/result"
 import { useQuery } from "@tanstack/react-query"
-import React, { useCallback, useEffect, useMemo, useState } from "react"
+import React, { useEffect, useMemo, useState } from "react"
 
-import { Navigate, useParams, useSearchParams } from "react-router-dom"
+import { useParams } from "react-router-dom"
 
-import { ComputeResult } from "./ComputeResult"
-import { LinkedMessages } from "./LinkedMessages"
-import { MessageData } from "./MessageData"
-import { ResultingMessages } from "./ResultingMessages"
 import { EntityBlock } from "@/components/EntityBlock"
-import { ChartDataItem, Graph } from "@/components/Graph"
 import { IdBlock } from "@/components/IdBlock"
 
 import { LoadingSkeletons } from "@/components/LoadingSkeletons"
 import { SectionInfo } from "@/components/SectionInfo"
-import { SectionInfoWithChip } from "@/components/SectionInfoWithChip"
 import { Subheading } from "@/components/Subheading"
-import { TabWithCount } from "@/components/TabWithCount"
-import { TagsSection } from "@/components/TagsSection"
 
 import { getMessageById, getResultingMessages } from "@/services/messages-api"
 import { AoMessage, Tag } from "@/types"
-import { truncateId } from "@/utils/data-utils"
 import { formatFullDate, formatRelative } from "@/utils/date-utils"
 
 import { formatNumber } from "@/utils/number-utils"
 import { isArweaveId } from "@/utils/utils"
 import { result } from "@permaweb/aoconnect"
-import { prettifyResult } from "@/utils/ao-utils"
 import { TokenAmountSection } from "@/components/TokenAmountSection"
 import { useTokenInfo } from "@/hooks/useTokenInfo"
 
@@ -45,11 +33,26 @@ interface Swap {
   tokenIn: string
   tokenOut: string
   initiator: string
+  lp: string
   amm: string
   quantityIn: string
   quantityOut: string
   feeBps: number
+  totalFee: string
 }
+
+const getResultingMessageByTags = (resultingMessages: any[], tags: [string, string][]) => {
+  return resultingMessages.find((m) => {
+    const { Tags } = m
+    const requiredTags = new Map(tags)
+    return [...requiredTags].every(([key, value]) =>
+      Tags.some(({ name, value: tagValue }: Tag) => name === key && tagValue === value),
+    )
+  })
+}
+
+const getTagValue = (tags: Tag[], tagName: string): string =>
+  tags.find((tag: Tag) => tag.name === tagName)?.value ?? ""
 
 export function SwapPage() {
   const { messageId = "" } = useParams()
@@ -78,29 +81,21 @@ export function SwapPage() {
       // If original message has no Swap tag => not a swap
       if (transferInMessage.tags["X-Action"] !== "Swap")
         throw new Error("Message does not initiate a swap")
-      // A - GET CREDIT NOTICE TO AMM
+      // A - GET CREDIT NOTICE TO LP
       // Get resulting messages from original message
       const { Messages: originalResultingMessages } = await result({
         message: transferInMessage.id,
         process: transferInMessage.to,
       })
-      // Get resulting Credit Notice message from the token in to the AMM
-      const creditNotice = originalResultingMessages.find((m) => {
-        const { Tags } = m
-        const requiredTags = new Map([
-          ["Action", "Credit-Notice"],
-          ["X-Action", "Swap"],
-        ])
-        return [...requiredTags].every(([key, value]) =>
-          Tags.some(({ name, value: tagValue }: Tag) => name === key && tagValue === value),
-        )
-      })
-      // If no credit notice message is sent to the AMM => not a swap
-      if (!creditNotice) throw new Error("No credit notice message sent to AMM")
+      // Get resulting Credit Notice message from the token in to the LP
+      const creditNotice = getResultingMessageByTags(originalResultingMessages, [
+        ["Action", "Credit-Notice"],
+        ["X-Action", "Swap"],
+      ])
+      // If no credit notice message is sent to the LP => not a swap
+      if (!creditNotice) throw new Error("No credit notice message sent to LP")
       // Get credit notice message ID from graph using message ref
-      const { value: creditNoticeRef } = creditNotice.Tags.find(
-        (tag: Tag) => tag.name === "Reference",
-      )
+      const creditNoticeRef = getTagValue(creditNotice.Tags, "Reference")
       const [, [creditNoticeMessage]] = await getResultingMessages(
         1,
         "",
@@ -112,36 +107,34 @@ export function SwapPage() {
       const {
         id: creditNoticeMessageId,
         tags: { Quantity: quantityIn },
-        to: ammProcessId,
+        to: lpProcessId,
       } = creditNoticeMessage
 
       // B - GET TRANSFER TO TOKEN OUT
       // Get resulting messages from credit notice to the AMM
       const { Messages: creditNoticeResultingMessages } = await result({
         message: creditNoticeMessageId,
-        process: ammProcessId,
+        process: lpProcessId,
       })
       // Get resulting Transfer message from the AMM to the token out
-      const transferOut = creditNoticeResultingMessages.find((m) => {
-        const { Tags } = m
-        const requiredTags = new Map([
-          ["Action", "Transfer"],
-          ["X-Action", "Swap-Output"],
-        ])
-        return [...requiredTags].every(([key, value]) =>
-          Tags.some(({ name, value: tagValue }: Tag) => name === key && tagValue === value),
-        )
-      })
+      const transferOut = getResultingMessageByTags(creditNoticeResultingMessages, [
+        ["Action", "Transfer"],
+        ["X-Action", "Swap-Output"],
+      ])
+      // Get resulting Order Confirmation from the pool to the AMM
+      const orderConfirmation = getResultingMessageByTags(creditNoticeResultingMessages, [
+        ["Action", "Order-Confirmation"],
+      ])
+      const totalFee = getTagValue(orderConfirmation.Tags, "Total-Fee")
+
       // If no transfer message is sent from the AMM => not a swap
       if (!transferOut) throw new Error("No transfer message sent from AMM to token out")
-      console.log(`transferOut:`, transferOut)
       // Get transfer message ID from graph using message ref
-      const { value: transferRef } = transferOut.Tags.find((tag: Tag) => tag.name === "Reference")
-      const { value: feeBpsStr } = transferOut.Tags.find((tag: Tag) => tag.name === "X-Fee-Bps")
-      const [, [transferOutMessage]] = await getResultingMessages(1, "", true, "", ammProcessId, [
+      const transferRef = getTagValue(transferOut.Tags, "Reference")
+      const feeBpsStr = getTagValue(transferOut.Tags, "X-Fee-Bps")
+      const [, [transferOutMessage]] = await getResultingMessages(1, "", true, "", lpProcessId, [
         transferRef,
       ])
-      console.log(`transferOutMessage:`, transferOutMessage)
       const {
         id: transferOutMessageId,
         tags: { Quantity: quantityOut },
@@ -157,12 +150,13 @@ export function SwapPage() {
         tokenIn: transferInMessage.to,
         tokenOut,
         initiator: transferInMessage.from,
-        amm: ammProcessId,
+        lp: lpProcessId,
+        amm: orderConfirmation.Target,
         quantityIn,
         quantityOut,
-        feeBps: Number(feeBpsStr)
+        feeBps: Number(feeBpsStr),
+        totalFee,
       }
-      console.log(`data:`, data)
 
       setSwapData(data)
     } catch (e) {
@@ -174,7 +168,6 @@ export function SwapPage() {
 
   useEffect(() => {
     if (!!message) {
-      console.log(`message:`, message)
       getSwap(message)
     }
   }, [message])
@@ -200,10 +193,12 @@ export function SwapPage() {
     tokenIn,
     tokenOut,
     initiator,
+    lp,
     amm,
     quantityIn,
     quantityOut,
     feeBps,
+    totalFee,
   } = swapData
 
   return (
@@ -213,20 +208,20 @@ export function SwapPage() {
         <Stack gap={4}>
           <SectionInfo title="Swapper" value={<EntityBlock entityId={initiator} />} />
           <SectionInfo title="AMM" value={<EntityBlock entityId={amm} />} />
+          <SectionInfo title="Liquidity Pool" value={<EntityBlock entityId={lp} />} />
           <SectionInfo title="Token In" value={<EntityBlock entityId={tokenIn} />} />
           <SectionInfo title="Token Out" value={<EntityBlock entityId={tokenOut} />} />
           <SectionInfo
             title="Fee"
             value={
-
-                <Tooltip title={`${feeBps} bps`}>
-                  <span>{feeBps * 0.01}%</span>
-                </Tooltip>
-              
+              <Tooltip title={`${feeBps} bps`}>
+                <span>{feeBps * 0.01}%</span>
+              </Tooltip>
             }
           />
           <TokenAmountSection tokenInfo={tokenInInfo} amount={quantityIn} label="Quantity In" />
           <TokenAmountSection tokenInfo={tokenOutInfo} amount={quantityOut} label="Quantity Out" />
+          <TokenAmountSection tokenInfo={tokenInInfo} amount={totalFee} label="Total Fee" />
           <SectionInfo
             title="Block Height"
             value={
